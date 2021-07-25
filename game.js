8804b511374566339183bf61205049f58981eb74
StevenWuTG <stevenwu91@icloud.com>
2021-07-25 17:28:11 -0400

    animation rolling

--- game.js
+++ game.js
@@ -24,10 +24,29 @@ let ballX = 250
 let ballY = 350
 const ballRadius = 5
 
+// speed
+let speedY
+let speedX
+let trajectoryX
+let computerSpeed
+
+// change Mobile Settings
+if (isMobile.matches) {
+  speedY = -2
+  speedX = speedY
+  computerSpeed = 4
+} else {
+  speedY = -1
+  speedX = speedY
+  computerSpeed = 3
+}
+
 // Score
 let playerScore = 0
-let botScore = 0
+let computerScore = 0
 const winningScore = 7
+let isGameOver = true
+let isNewGame = true
 
 
 //generating canvas
@@ -76,4 +95,118 @@ const createCanvas = () => {
     renderCanvas()
 }
 
-createCanvas()
+const botAI = () => {
+    if (playerMoved) {
+        if (paddleTopX + paddleDiff < ballX) {
+            paddleTopX += computerSpeed
+        } else {
+            paddleTopX -= computerSpeed
+        }
+    }
+}
+
+const ballMove = () => {
+    // Vertical Speed
+    ballY += - speedY
+    // Horizontal Speed
+    if (playerMoved && paddleContact) {
+        ballX += speedX
+    }
+}
+
+const ballReset = () => {
+    ballX = width / 2
+    ballY = height / 2
+    speedY = -3
+    paddleContact = false
+}
+
+const ballBoundaries = () => {
+    // Bounce off Left Wall
+    if (ballX < 0 && speedX < 0) {
+      speedX = -speedX
+    }
+    // Bounce off Right Wall
+    if (ballX > width && speedX > 0) {
+      speedX = -speedX
+    }
+    // Bounce off player paddle (bottom)
+    if (ballY > height - paddleDiff) {
+      if (ballX > paddleBottomX && ballX < paddleBottomX + paddleWidth) {
+        paddleContact = true
+        // Add Speed on Hit
+        if (playerMoved) {
+          speedY -= 1
+          // Max Speed
+          if (speedY < -5) {
+            speedY = -5
+            computerSpeed = 6
+          }
+        }
+        speedY = -speedY
+        trajectoryX = ballX - (paddleBottomX + paddleDiff)
+        speedX = trajectoryX * 0.3
+      } else if (ballY > height) {
+        // Reset Ball, add to Computer Score
+        ballReset()
+        botScore++
+      }
+    }
+    // Bounce off computer paddle (top)
+    if (ballY < paddleDiff) {
+      if (ballX > paddleTopX && ballX < paddleTopX + paddleWidth) {
+        // Add Speed on Hit
+        if (playerMoved) {
+          speedY += 1
+          // Max Speed
+          if (speedY > 5) {
+            speedY = 5
+          }
+        }
+        speedY = -speedY
+      } else if (ballY < 0) {
+        // Reset Ball, add to Player Score
+        ballReset()
+        playerScore++
+      }
+    }
+}
+
+
+
+
+const animate = () => {
+    renderCanvas()
+    ballMove()
+    ballBoundaries()
+    botAI()
+    window.requestAnimationFrame(animate)
+
+}
+
+const startGame = () => {
+    playerScore = 0
+    botScore = 0
+    ballReset()
+
+    createCanvas()
+    animate()
+
+    canvas.addEventListener('mousemove', (e) => {
+        playerMoved = true
+        // Compensate for canvas being centered
+        paddleBottomX = e.clientX - canvasPosition - paddleDiff
+        if (paddleBottomX < paddleDiff) {
+          paddleBottomX = 0
+        }
+        if (paddleBottomX > width - paddleWidth) {
+          paddleBottomX = width - paddleWidth
+        }
+        // Hide Cursor
+        canvas.style.cursor = 'none'
+    })
+    
+
+}
+
+startGame()
