e9dffc65d22ae2e4f6658b10f88d24018735b7be
StevenWuTG <stevenwu91@icloud.com>
2021-07-25 16:15:21 -0400

    canvas elements complete

--- game.js
+++ game.js
@@ -19,6 +19,16 @@ let paddleTopX = 225
 let playerMoved = false
 let paddleContact = false
 
+//ball code
+let ballX = 250
+let ballY = 350
+const ballRadius = 5
+
+// Score
+let playerScore = 0
+let botScore = 0
+const winningScore = 7
+
 
 //generating canvas
 
@@ -35,6 +45,27 @@ const renderCanvas = () => {
 
     //bot paddle
     context.fillRect(paddleTopX, 10, paddleWidth, paddleHeight)
+
+    //middle line
+    context.beginPath()
+    context.setLineDash([4])
+    context.moveTo(0, 350)
+    context.lineTo(500,350)
+    context.strokeStyle = 'lime'
+    context.stroke()
+
+    //ball 
+    context.beginPath()
+    context.arc(ballX, ballY, ballRadius, 2 * Math.PI, false)
+    context.fillStyle = 'white'
+    context.fill()
+
+    //score
+
+    context.font = '32px Courier New'
+    context.fillText(playerScore, 20, canvas.height / 2 + 50)
+    context.fillText(botScore, 20, canvas.height / 2 - 30)
+
 
 }
 
